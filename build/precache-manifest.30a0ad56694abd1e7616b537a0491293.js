c4e076b32dd1f3f483d9ba7f8f787fe921dce856
TeeVy <carlorobin@gmail.com>
2020-03-26 22:24:42 +0100

    Add build folder

---
+++ build/precache-manifest.30a0ad56694abd1e7616b537a0491293.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b81654b2bc1bbf3781f81aa0099e5b1a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1f9740ee0b3fd34fecd",
+    "url": "/static/css/main.f8044302.chunk.css"
+  },
+  {
+    "revision": "0c08bbdcdfae0e021043",
+    "url": "/static/js/2.741404e6.chunk.js"
+  },
+  {
+    "revision": "2549765df2eb04fa59a1f28130514f65",
+    "url": "/static/js/2.741404e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1f9740ee0b3fd34fecd",
+    "url": "/static/js/main.025d7bb8.chunk.js"
+  },
+  {
+    "revision": "26ca1eb742dd7afbed63",
+    "url": "/static/js/runtime-main.33fd05c1.js"
+  }
+]);
